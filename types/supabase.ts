d59fcbe886eee57c2c4be0f5aee0ec30aed55fd4
Ahmed1870 <xcm3108@gmail.com>
2026-03-27 06:55:39 +0200

    fix: generate standalone types to resolve database/index missing error

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,1 +1,33 @@
-export * from '../database/index';
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      [_ in string]: {
+        Row: { [_ in string]: any }
+        Insert: { [_ in string]: any }
+        Update: { [_ in string]: any }
+      }
+    }
+    Views: {
+      [_ in string]: {
+        Row: { [_ in string]: any }
+      }
+    }
+    Functions: {
+      [_ in string]: {
+        Args: { [_ in string]: any }
+        Returns: any
+      }
+    }
+    Enums: {
+      [_ in string]: any
+    }
+  }
+}
